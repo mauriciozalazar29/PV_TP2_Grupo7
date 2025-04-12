16cc4b9570e6f18f627ea5107f5890f32b7a4377
maurizz29 <mauriitomas@gmail.com>
2025-04-12 11:36:53 -0300

    agregado de  html para ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,11 @@
+const calcularMayor = (numero1, numero2) => {
+    if ( numero1 > numero2 ) {
+        alert(numero1 + " es mayor que " + numero2);
+    } else if (numero2 > numero1){
+        alert(numero2 + " es mayor que " + numero1);
+    }else{
+        alert("Los numeros " + numero1 + " y " + numero2 + " son iguales");
+    }
+}
+
+calcularMayor(1,10);
